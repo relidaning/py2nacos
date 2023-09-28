8198007b0e013b5e287696932718cc87e7a59e63
lidaning <453882101@qq.com>
2023-09-28 15:20:40 +0800

    initializition

---
+++ py_request_nacos/__init__.py
@@ -0,0 +1,38 @@
+import requests
+import json
+import numpy as np
+
+register_service_suffix='/v1/ns/instance'
+heartbeat_suffix='/v1/ns/instance/beat'
+request_service_suffix='/v1/ns/instance/list'
+
+
+
+
+def request_from_nacos(nacos_url, service_name):
+    result = requests.get(nacos_url + request_service_suffix, params={'serviceName': service_name}).text
+    result_json = json.loads(result)
+    hosts = result_json['hosts']
+    host = hosts[np.random.randint(0, len(hosts))]
+    return 'http://'+host['ip']+':'+str(host['port'])
+
+
+def register_to_nacos(nacos_url, service_name, ip, port):
+    result = requests.post(nacos_url + register_service_suffix,
+                           data={'serviceName': service_name, 'ip': ip, 'port': port}).text
+    return result
+
+
+def heartbeat_to_nacos(nacos_url, service_name, ip, port):
+    result = requests.post(nacos_url + heartbeat_suffix,
+                           data={'serviceName': service_name, 'ip': ip, 'port': port}).text
+    return result
+
+
+nacos_url='http://82.157.147.8:8848/nacos'
+service_name='xxx'
+print(register_to_nacos(nacos_url, service_name, '127.0.0.1', '5020'))
+import time
+while(True):
+    print(heartbeat_to_nacos(nacos_url, service_name, '127.0.0.1', '5020'))
+    time.sleep(5)
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='py-request-nacos',
+    version='0.0.4',
+    author='lidaning',
+    author_email='453882101@qq.com',
+    description='request service from nacos',
+    packages=['py_request_nacos'],
+    install_requires=[
+        'requests',
+        'numpy',
+    ],
+)
