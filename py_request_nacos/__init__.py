d37333781ecf3e18f16889488e6976aa4fcb1f2d
lidaning <41282656+relidaning@users.noreply.github.com>
2023-09-28 17:36:27 +0800

    auto heartbeat when registerd

--- py_request_nacos/__init__.py
+++ py_request_nacos/__init__.py
@@ -18,7 +18,9 @@ def request_from_nacos(nacos_url, service_name):
 def register_to_nacos(nacos_url, service_name, ip, port):
     result = requests.post(nacos_url + register_service_suffix,
                            data={'serviceName': service_name, 'ip': ip, 'port': port}).text
-    heartbeat_to_nacos(nacos_url, service_name, ip, port)
+    while True:
+        heartbeat_to_nacos(nacos_url, service_name, ip, port)
+        time.sleep(5)
     return result
 
 
