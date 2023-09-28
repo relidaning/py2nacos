49bafd25ef3f25d99d28be02f4993d522c728cac
lidaning <41282656+relidaning@users.noreply.github.com>
2023-09-28 16:55:33 +0800

    upgrade

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='py-request-nacos',
-    version='0.0.4',
+    version='0.0.5',
     author='lidaning',
     author_email='453882101@qq.com',
     description='request service from nacos',
